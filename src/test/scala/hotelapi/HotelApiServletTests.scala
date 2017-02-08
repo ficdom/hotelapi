7c564a3f72bd4f3b98580f3218d34b3c0e41595d
Dominik Fic <ficdom@gmail.com>
2017-02-08 22:11:30 +0100

    UT cleanup

--- src/test/scala/hotelapi/HotelApiServletTests.scala
+++ src/test/scala/hotelapi/HotelApiServletTests.scala
@@ -6,6 +6,11 @@ import org.scalatra.test.scalatest.ScalatraSuite
 class HotelApiServletTests extends ScalatraSuite with FunSuiteLike {
   addServlet(classOf[HotelApiServlet], "/*")
 
+  private val bangkokHotel1 = """{"hotelId":1,"roomType":"Deluxe","price":1000.0}"""
+  private val bangkokHotel6 = """{"hotelId":6,"roomType":"Superior","price":2000.0}"""
+  private val bangkokHotel8 = """{"hotelId":8,"roomType":"Superior","price":2400.0}"""
+  private val amsterdamHotel = """{"hotelId":2,"roomType":"Superior","price":2000.0}"""
+
   test("Call /hotels endpoint without the api key") {
     get("/hotels/Bangkok") {
       status should equal(403)
@@ -16,30 +21,21 @@ class HotelApiServletTests extends ScalatraSuite with FunSuiteLike {
   test("Retrieve hotels without sorting") {
     get("/hotels/Bangkok?key=abc") {
       status should equal(200)
-      body should equal(
-        """[{"hotelId":6,"roomType":"Superior","price":2000.0},""" +
-          """{"hotelId":1,"roomType":"Deluxe","price":1000.0},""" +
-          """{"hotelId":8,"roomType":"Superior","price":2400.0}]""")
+      body should equal(s"[$bangkokHotel6,$bangkokHotel1,$bangkokHotel8]")
     }
   }
 
   test("Retrieve hotels sorted ascending") {
     get("/hotels/Bangkok?key=abcd&sort=asc") {
       status should equal(200)
-      body should equal(
-        """[{"hotelId":1,"roomType":"Deluxe","price":1000.0},""" +
-          """{"hotelId":6,"roomType":"Superior","price":2000.0},""" +
-          """{"hotelId":8,"roomType":"Superior","price":2400.0}]""")
+      body should equal(s"[$bangkokHotel1,$bangkokHotel6,$bangkokHotel8]")
     }
   }
 
   test("Retrieve hotels sorted descending") {
     get("/hotels/Bangkok?key=abcde&sort=desc") {
       status should equal(200)
-      body should equal(
-        """[{"hotelId":8,"roomType":"Superior","price":2400.0},""" +
-          """{"hotelId":6,"roomType":"Superior","price":2000.0},""" +
-          """{"hotelId":1,"roomType":"Deluxe","price":1000.0}]""")
+      body should equal(s"[$bangkokHotel8,$bangkokHotel6,$bangkokHotel1]")
     }
   }
 
@@ -53,7 +49,7 @@ class HotelApiServletTests extends ScalatraSuite with FunSuiteLike {
   test("Retrieve hotels repeatedly") {
     get("/hotels/Amsterdam?key=1234") {
       status should equal(200)
-      body should equal("""[{"hotelId":2,"roomType":"Superior","price":2000.0}]""")
+      body should equal(s"[$amsterdamHotel]")
     }
     get("/hotels/Amsterdam?key=1234") {
       status should equal(403)
@@ -68,22 +64,22 @@ class HotelApiServletTests extends ScalatraSuite with FunSuiteLike {
   test("Retrieve hotels repeatedly for a key with a high rate limit") {
     get("/hotels/Amsterdam?key=54fea") {
       status should equal(200)
-      body should equal("""[{"hotelId":2,"roomType":"Superior","price":2000.0}]""")
+      body should equal(s"[$amsterdamHotel]")
     }
     get("/hotels/Amsterdam?key=54fea") {
       status should equal(200)
-      body should equal("""[{"hotelId":2,"roomType":"Superior","price":2000.0}]""")
+      body should equal(s"[$amsterdamHotel]")
     }
     get("/hotels/Amsterdam?key=54fea") {
       status should equal(200)
-      body should equal("""[{"hotelId":2,"roomType":"Superior","price":2000.0}]""")
+      body should equal(s"[$amsterdamHotel]")
     }
   }
 
   test("Retrieve hotels even when the city name is weirdly cased") {
     get("/hotels/amSTeRdaM?key=54fea") {
       status should equal(200)
-      body should equal("""[{"hotelId":2,"roomType":"Superior","price":2000.0}]""")
+      body should equal(s"[$amsterdamHotel]")
     }
   }
 }
